629a390e194eb8ffffec5a998f57768d5a56f677
Eddington Rowellis <eddingtonibi@gmail.com>
2023-03-23 09:29:11 +0300

    commit tree 8af361d804d27bbffdb509ef5e13aa073280eda9

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,11 @@
 # Your code goes here!
+class Anagram
+  
+  def initialize(word)
+    @word = word
+  end
+
+  def match(words)
+    words.select { |w| w.chars.sort == @word.chars.sort }
+  end
+end
